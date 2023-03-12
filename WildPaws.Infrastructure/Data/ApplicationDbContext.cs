0110aace042b6d370720a4a67bab2ffb2cd77aad
GeorgiTopalov <g.s.topalov@gmail.com>
2023-03-12 13:32:41 +0700

    Moved Data to inftastructure

--- WildPaws.Infrastructure/Data/ApplicationDbContext.cs
+++ WildPaws.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace WildPaws.Data
+namespace WildPaws.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- WildPaws.Infrastructure/Migrations/00000000000000_CreateIdentitySchema.cs
+++ WildPaws.Infrastructure/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace WildPaws.Data.Migrations
+namespace WildPaws.Infrastructure.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
