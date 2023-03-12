9c53b603a0f3047e1f0aaf9b13ef3676e6d27017
GeorgiTopalov <g.s.topalov@gmail.com>
2023-03-12 09:23:37 +0700

    Scafolded Identity

---
+++ WildPaws/Areas/Identity/Data/WildPawsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WildPaws.Areas.Identity.Data;
+
+namespace WildPaws.Data;
+
+public class WildPawsContext : IdentityDbContext<WildPawsUser>
+{
+    public WildPawsContext(DbContextOptions<WildPawsContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
