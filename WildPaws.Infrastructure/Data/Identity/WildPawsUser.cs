e4644d62a558341f8d17fbccba85dd31a3c50dfd
GeorgiTopalov <g.s.topalov@gmail.com>
2023-03-14 09:25:22 +0700

    Created login form

--- WildPaws.Infrastructure/Data/Identity/WildPawsUser.cs
+++ WildPaws.Infrastructure/Data/Identity/WildPawsUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,10 @@ namespace WildPaws.Infrastructure.Data.Identity;
 // Add profile data for application users by adding properties to the WildPawsUser class
 public class WildPawsUser : IdentityUser
 {
+    [Required]
+    public  string UserName { get; set; }
+
+    [Required]
+    public string Password { get; set; }
 }
 
