cc89f03b2f054b624888698dc999e63e09353c09
GeorgiTopalov <g.s.topalov@gmail.com>
2023-03-14 13:42:22 +0700

    Register form complete. Register and login fully working.

--- WildPaws.Infrastructure/Data/Identity/WildPawsUser.cs
+++ WildPaws.Infrastructure/Data/Identity/WildPawsUser.cs
@@ -10,10 +10,6 @@ namespace WildPaws.Infrastructure.Data.Identity;
 // Add profile data for application users by adding properties to the WildPawsUser class
 public class WildPawsUser : IdentityUser
 {
-    [Required]
-    public  string UserName { get; set; }
 
-    [Required]
-    public string Password { get; set; }
 }
 
