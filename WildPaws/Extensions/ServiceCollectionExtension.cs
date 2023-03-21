714d0fbd1f17206c1dc2a55b06185f7f6d3a847c
GeorgiTopalov <g.s.topalov@gmail.com>
2023-03-21 08:42:36 +0700

    Added ServiceCollectionExtension and moved services away from Program

---
+++ WildPaws/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WildPaws.Infrastructure.Data;
+using WildPaws.Infrastructure.Data.Identity;
+using WildPaws.Infrastructure.Data.Repositories;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services)
+        {
+            services.AddDefaultIdentity<WildPawsUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+            return services;
+        }
+
+    }
+}
