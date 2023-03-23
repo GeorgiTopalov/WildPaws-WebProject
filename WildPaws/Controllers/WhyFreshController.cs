7862cbaeca7be36dc841c7c006b6ac862ead1dfb
GeorgiTopalov <g.s.topalov@gmail.com>
2023-03-23 09:27:31 +0700

    Added views and controllers for all links on the navbar.

---
+++ WildPaws/Controllers/WhyFreshController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WildPaws.Controllers
+{
+    public class WhyFreshController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
