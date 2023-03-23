785bc980d5b0ed857dce66cead2dc9698f333fa9
GeorgiTopalov <g.s.topalov@gmail.com>
2023-03-23 14:00:59 +0700

    Some html and css improvements

---
+++ WildPaws/Controllers/QuestionnaireController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WildPaws.Controllers
+{
+    public class QuestionnaireController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
