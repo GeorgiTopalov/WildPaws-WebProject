55e5b68110d7cc9ddbe5e92ea0e1ff5daf304665
Georgi Topalov <g.s.topalov@gmail.com>
2023-08-24 09:15:30 +0700

    Added NUnit Test project using Sqlite.

---
+++ WildPaws.Test/InMemoryDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildPaws.Infrastructure.Data;
+
+namespace WildPaws.Test
+{
+    public class InMemoryDbContext
+    {
+        private readonly SqliteConnection connection;
+        private readonly DbContextOptions<ApplicationDbContext> dbContextOptions;
+
+        public InMemoryDbContext()
+        {
+            connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using var context = new ApplicationDbContext(dbContextOptions);
+
+            context.Database.EnsureCreated();
+        }
+    }
+}
