0943ddb9d1afec6a87794ed79fa785d6f1359dc7
Georgi Topalov <g.s.topalov@gmail.com>
2023-09-12 09:41:08 +0700

    Fixed ForYourPetController name

--- WildPaws/Controllers/ForYourPetController.cs
+++ WildPaws/Controllers/ForYourPetController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using WildPaws.Core.Contracts;
 using WildPaws.Core.Models;
 
 namespace WildPaws.Controllers
 {
-    public class ForYourPet : BaseController
+    public class ForYourPetController : BaseController
     {
+        private readonly IQuestionnareService service;
+
+
+        public ForYourPetController(IQuestionnareService service)
+        {
+            this.service = service;
+        }
         public IActionResult Index()
         {
+
             if (Request.Cookies.TryGetValue("QuestionnaireData", out var questionnaireData))
             {
                 var model = JsonConvert.DeserializeObject<QuestionnaireViewModel>(questionnaireData);
