24997c5c12676c8e9b7ba744fe322d7dfe27c538
Georgi Topalov <g.s.topalov@gmail.com>
2023-09-14 10:34:12 +0700

    Optimized the code in ForYourPetController

--- WildPaws/Controllers/ForYourPetController.cs
+++ WildPaws/Controllers/ForYourPetController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using WildPaws.Core.Contracts;
 using WildPaws.Core.Models;
+using WildPaws.Infrastructure.Data;
 
 namespace WildPaws.Controllers
 {
@@ -14,41 +15,38 @@ namespace WildPaws.Controllers
         {
             this.service = service;
         }
+        
+
         public async Task<IActionResult> Index()
         {
-
             if (Request.Cookies.TryGetValue("QuestionnaireData", out var questionnaireData))
             {
                 var model = JsonConvert.DeserializeObject<QuestionnaireViewModel>(questionnaireData);
-
                 var recommendedRecipes = await service.RecommendedRecipes(model);
+
                 ViewBag.Recipes = recommendedRecipes;
-
-                var recipeNamesString = string.Join(", ", recommendedRecipes.Select(recipe => recipe.RecipeName));
-                ViewBag.RecipeNamesString = recipeNamesString;
-
-                var ingredientNamesList = new List<string>();
-                foreach (var recipe in recommendedRecipes)
-                {
-                    ingredientNamesList.Add(string.Join(", ", recipe.Ingredients.Select(ingredient => ingredient.Name)));
-                }
-                ViewBag.IngredientNamesList = ingredientNamesList;
-
-
-                var averageCalories = await service.CalculateAverageCalories(recommendedRecipes);
-                ViewBag.AverageCalories = averageCalories;
-
-                var gramsToConsume = await service.GramsToConsume(model, averageCalories);
-                ViewBag.GramsToConsume = gramsToConsume;
-
-                var pricePerDay = await service.PricePerDay(gramsToConsume);
-                ViewBag.PricePerDay = pricePerDay;
+                ViewBag.RecipeNamesString =  GetRecipeNamesString(recommendedRecipes);
+                ViewBag.IngredientNamesList =  GetIngredientNamesList(recommendedRecipes);
+                ViewBag.AverageCalories = await service.CalculateAverageCalories(recommendedRecipes);
+                ViewBag.GramsToConsume = await service.GramsToConsume(model, ViewBag.AverageCalories);
+                ViewBag.PricePerDay = await service.PricePerDay(ViewBag.GramsToConsume);
 
                 return View(model);
             }
 
             return View(new QuestionnaireViewModel());
-
         }
+
+        private string GetRecipeNamesString(List<Recipe> recipes)
+        {
+            return string.Join(", ", recipes.Select(recipe => recipe.RecipeName));
+        }
+
+        private List<string> GetIngredientNamesList(List<Recipe> recipes)
+        {
+            return recipes.Select(recipe => string.Join(", ", recipe.Ingredients.Select(ingredient => ingredient.Name))).ToList();
+        }
+
+
     }
 }
